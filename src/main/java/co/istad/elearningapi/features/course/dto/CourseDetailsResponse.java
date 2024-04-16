91f90167798c6c895f87138c8fcd22cba3057aa1
jipordev <srengchipor99@gmail.com>
2024-04-16 22:54:34 +0700

    fix course response

--- src/main/java/co/istad/elearningapi/features/course/dto/CourseDetailsResponse.java
+++ src/main/java/co/istad/elearningapi/features/course/dto/CourseDetailsResponse.java
@@ -7,7 +7,7 @@ import co.istad.elearningapi.features.instructor.dto.InstructorResponse;
 public record CourseDetailsResponse(
         String alias,
         String description,
-        Boolean isFree,
+        boolean isFree,
         String thumbnail,
         String title,
         InstructorResponse instructor,
