6c0771ac4e2380d552575e68d794f2c2b40f817d
jipordev <srengchipor99@gmail.com>
2024-04-17 16:25:12 +0700

    pull from instructor-management

--- src/main/java/co/istad/elearningapi/features/instructor/InstructorController.java
+++ src/main/java/co/istad/elearningapi/features/instructor/InstructorController.java
@@ -5,6 +5,7 @@ import co.istad.elearningapi.features.user.dto.UserCreateRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 public class InstructorController {
     private final InstructorService instructorService;
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     void createNew(@Valid @RequestBody InstructorCreateRequest instructorCreateRequest) {
         instructorService.createNew(instructorCreateRequest);
